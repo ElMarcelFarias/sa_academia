c074ba4b9250aa71318b1410e28551837f3e3090
MARCEL LEITE DE FARIAS <marcel_farias@estudante.sesisenai.org.br>
2022-06-28 19:31:06 -0300

    BugFix: Erros no cadastro de venda!

--- php/index.php
+++ php/index.php
@@ -46,7 +46,7 @@
                     <td><?= $row['data']?></td>
                     <td><?= $row['formaPagamento']?></td>
                     <td>
-                        <!--<button type="button" class="btn btn-warning btn-sm updateUser" id="<?= //$row['idVenda']?>"><span class="material-icons align-text-bottom">edit</span></button>-->
+                        <!--<button type="button" class="btn btn-warning btn-sm updateUser" id=""><span class="material-icons align-text-bottom">edit</span></button>-->
                         <button type="button" class="btn btn-danger btn-sm deleteSell" id="<?=$row['idVenda']?>"><span class="material-icons align-text-bottom">close</span></button>
                     </td>
                 </tr>
