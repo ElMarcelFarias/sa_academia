8fc540e5e7b041f8a40bef70d73007b3914b6ad9
Otávio Henrique Vicente <101840512+otavio-vicente@users.noreply.github.com>
2022-07-11 19:49:09 -0300

    New: Funcionalidade do Logout

---
+++ php/efetuarLogout.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+unset($_SESSION['nivel']);
+
+header("Location: ../index.html");
+
+
+
+
+?>
--- php/newCadPlanos.php
+++ php/newCadPlanos.php
@@ -79,6 +79,12 @@
               <ul>
                   <li><a href="https://github.com/ElMarcelFarias/sa_academia" title="Documentação" target="_blank"><span class="nav-icon material-icons">school</span> Documentação</a></li>
                   <li><form action="gerarPDFPlanos.php"><button type="" class="btn text-center" style="background-color: transparent; color: #6c757d;"><span class="ml-1 mr-2 nav-icon material-icons">picture_as_pdf</span>Gerar PDF</button></form></li>
+                  <li><a href="efetuarLogout.php"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                        </svg> Logout
+                      </a>
+                  </li>
               </ul>
             </nav>
 
