2dd135a33e2183bbcd89cfccff1949af308c8d83
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-07-20 08:29:47 -0300

    Change: Arrumando sql da base de dados!

--- base/academia.sql
+++ base/academia.sql
@@ -7,15 +7,6 @@
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `academia`
@@ -26,7 +17,8 @@ SET time_zone = "+00:00";
 --
 -- Estrutura da tabela `alunos`
 --
-create table academia;
+create database academia;
+
 CREATE TABLE `alunos` (
   `idAluno` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
@@ -236,7 +228,3 @@ ALTER TABLE `vendas`
   ADD CONSTRAINT `fk_vendas_alunos1` FOREIGN KEY (`alunos_idAluno`) REFERENCES `alunos` (`idAluno`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_vendas_planos` FOREIGN KEY (`planos_idPlano`) REFERENCES `planos` (`idPlano`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
